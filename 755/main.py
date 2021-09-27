7f50906e1ba30ded62d9b014ef3e342a5889b573
parachvte <ryannx6@gmail.com>
2021-09-27 15:03:21 +0800

    Add some comments to P755

--- 755/main.py
+++ 755/main.py
@@ -1,33 +1,57 @@
+"""
+Problem link: https://projecteuler.net/problem=755
+
+Solution note:
+
+    The problem is to calculate all possible combinations of fibonacci numbers with
+    a sum smaller or equal to 10^13.
+
+    We search all Fibonacci numbers in descending order, subtract selected ones from
+    the total sum (in our case is 10^13), compare the sum difference, and record the
+    number of combinations.
+
+    Firstly, We have an inference that: f(n) = sum {f(i) | for i = 0..n-2} + 2
+    When the sum difference >= sum of all Fibonacci numbers left (f(n - 1) + f(n) -2),
+    all combinations of f(n - 1) ... f(0) are accepted, no more search needed.
+    That's 2^n combinations.
+
+    The second prune is LRU cache which reduces our calculation from 573881 to 108
+    times, execution time from near 2s to instant.
+
+    answer: 2877071595975576960
+
+    Maximum to calculate S(10**104) due to my maximum recursion depth is 500.
+"""
+
 from functools import lru_cache
 
-MAX_SUM = 10000000000000
+MAX_SUM = 10 ** 13
 fib = [1, 2]
-
-def has_bit(opt, i):
-    return 1 if (opt & (1 << i)) > 0 else 0
 
 @lru_cache
 def dfs(i, left):
+    # fib array can not be in the signature cuz lru_cache doesn't support array
     global fib
-    if left == 0:
+
+    if left == 0:  # nothing can be selected
         return 1
 
-    if i == -1 or i == 0:
+    if i == 0:  # I doubt we will reach here.
         return 1
 
-    fib_2 = fib[i] - 2
+    sum_left = fib[i - 1] + (fib[i] - 2)
 
     res = 0
-    for opt in range(4):
-        s = has_bit(opt, 1) * fib[i] + has_bit(opt, 0) * fib[i - 1]
-
-        if s > left:
+    for opt in range(2):
+        current = fib[i] if opt == 1 else 0
+        if current > left:
             break
 
-        if s + fib_2 <= left:
-            res += 1 << (i - 1)
+        # important prune
+        if current + sum_left <= left:
+            res += 1 << i
         else:
-            res += dfs(i - 2, left - s)
+            res += dfs(i - 1, left - current)
 
     return res
     
@@ -47,12 +71,12 @@ def main():
     print('count of fibonacci numbers =', fib_n)
     print(' '.join(map(str, fib)))
 
-    print('S({}) = {}'.format(100, dfs(fib_n - 1, 100))) 
-    print('S({}) = {}'.format(10000, dfs(fib_n - 1, 10 ** 4))) 
+    print('S({}) = {}'.format(100, dfs(fib_n - 1, 100)))
+    print('S({}) = {}'.format(10000, dfs(fib_n - 1, 10 ** 4)))
     print('S({}) = {}'.format(10 ** 13, dfs(fib_n - 1, 10 ** 13))) 
+    print('S({}) = {}'.format(MAX_SUM, dfs(fib_n - 1, MAX_SUM))) 
 
 
 if __name__ == '__main__':
     main()
 
-# answer: 2877071595975576960
